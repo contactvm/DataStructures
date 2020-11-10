d6453ed6ca1c7d273056afb36c2740b1af6d6f60
Viraj Mehta <virajmehta.contact@gmail.com>
2020-11-10 07:42:49 -0500

    get mod and add value to array size and divide size of array

--- Array/FindDuplicateNumberInArray.cs
+++ Array/FindDuplicateNumberInArray.cs
@@ -1,6 +1,7 @@
 //Missing Number in Array
 //mistakes 
 //1. Dictionary spell, access Dictionary and KeyValuePair Datatype for Iteration and access Key and Value when array
+//Missing Number in Array
 using System;
 using System.Collections.Generic;
 public class FindDuplicateArray{
@@ -32,12 +33,43 @@ public class FindDuplicateArray{
 			}
 		}
 	}
+	static void printRepeating(int[] arr, int n)
+    {
+        // First check all the values that are
+        // present in an array then go to that
+        // values as indexes and increment by
+        // the size of array
+        for (int i = 0; i < n; i++) 
+        {
+            int index = arr[i] % n;
+            //Console.WriteLine("index="+index.ToString()+"  arr[i]"+ arr[i].ToString());
+            arr[index] += n;
+        }
+        foreach(var i in arr)
+        {
+            Console.WriteLine(i.ToString());
+        }
+ 
+        // Now check which value exists more
+        // than once by dividing with the size
+        // of array
+        for (int i = 0; i < n; i++)
+        {
+            int t=(arr[i] / n);
+            //Console.WriteLine(t.ToString());
+            if ((arr[i] / n) >= 2)
+                {
+                    Console.Write(i + " ");
+                }
+        }
+    }
 	
 	public static void Main()
 	{
 		int[] a={1,2,3,2};
-		
+		int[] b={1,3,2,3,4};
 		FindDuplicateArrayFromArray(a);
+		printRepeating(b,b.Length);
 	}
 }
 
